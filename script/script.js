c428aecda5a92fbdbd59263e34b1af6ef85ada3a
Pauline Cvt <paulinecvt2021@gmail.com>
2024-01-29 14:47:35 +0100

    working on display obstacles

--- script/script.js
+++ script/script.js
@@ -29,61 +29,70 @@ class Player {
 
     moveDown (){
         this.positionY+= 10;
-    }
-/*
-    handleKeyDown(event) {
-        console.log('handleKeyDown called');
-        console.log('Event target:', event.target);
-        console.log('This context:', this);
-        if (event.target !== this.domElm) return;
-        if (event.key === 'ArrowUp') {
-            console.log('movetop function')
-            this.moveTop();
-        } else if (event.key === 'ArrowDown') {
-            console.log('movebottom function')
-            this.moveBottom();
-        }
-        
     };
 
-    moveTop() {
-        this.positionY -= 10;
-        this.domElm.style.top = this.positionY + '%';
-        console.log('Moved top to:', this.positionY);
-    };
-
-    moveBottom() {
-        this.positionY += 10;
-        this.domElm.style.top = this.positionY + '%';
-        console.log('Moved bottom to:', this.positionY);
-    };
-    */
 };
 
 
-// TRASHES CLASS
+//TRASHES CLASS
 class Trashes {
-    constructor (){
-        this.positionX = Math.floor(Math.random() * (100 - this.width + 1));
-        this.positionY = 100;
-        this.width = 20;
-        this.height = 20;
+    constructor (positionX, positionY, width, height) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.width = width;
+        this.height = height;
         this.domElm = null;
 
         this.createDomElm();
     };
-    
+
     createDomElm(){
         this.domElm = document.createElement("div");
         this.domElm.setAttribute("class", "trash");
-        this.domElm.style.width = this.width + "vw";
-        this.domElm.style.height = this.height + "vh"
-        this.domElm.style.left = this.positionX + "vw";
-        this.domElm.style.bottom = this.positionY + "vh";
+        this.domElm.style.width = this.width + "%";
+        this.domElm.style.height = this.height + "%"
+        this.domElm.style.left = this.positionX + "%";
+        this.domElm.style.bottom = this.positionY + "%";
         
         const boardElm = document.getElementById("board");
         boardElm.appendChild(this.domElm);
     };
+
+    moveLeft(){
+        this.positionX -= 10;
+        this.domElm.style.left = this.positionX + "%";
+        if (this.positionX < 0) {
+            this.positionX = 0;
+        }
+    };
+
+    generateObstacle () {
+        const positionX = Math.floor(Math.random() * (100 - this.width + 1));
+        const positionY = 100;
+        const width = 20;
+        const height = 20;
+        return new Trashes(positionX, positionY, width, height);
+    }
+
+    // move obstacles & detect collision
+    moveAndDetectCollision (trashes, player) {
+        trashes.forEach((obstacleInstance) => {
+
+            // 1. move current obstacle
+            obstacleInstance.moveLeft();
+
+            // 2. detect if there's a collision between the current obstacle and the player
+            if (player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+                player.positionX + player.width > obstacleInstance.positionX &&
+                player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+                player.positionY + player.height > obstacleInstance.positionY) {
+                console.log("game over");
+
+            }
+
+        });
+    }
+
 };
 
 // CONST
@@ -91,6 +100,7 @@ const player = new Player ();
 const trashes = [];
 
 // EVENTS
+// Move the player
 document.addEventListener("keydown", (e) => {
     if (e.code === 'ArrowUp') {
         player.moveUp();
@@ -100,28 +110,4 @@ document.addEventListener("keydown", (e) => {
     player.domElm.style.top = player.positionY + '%';
 });
 
-/*
-// generate obstacles 
-setInterval(() => {
-    const newObstacle = new Trashes();
-    trashes.push(newObstacle);
-}, 3000);
 
-// move obstacles & detect collision
-setInterval(() => {
-    trashes.forEach((obstacleInstance) => {
-
-        // 1. move current obstacle
-        obstacleInstance.moveDown();
-
-        // 2. detect if there's a collision between the current obstacle and the player
-        if (player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
-            player.positionX + player.width > obstacleInstance.positionX &&
-            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
-            player.positionY + player.height > obstacleInstance.positionY) {
-            console.log("game over");
-            location.href = "gameover.html";
-        }
-
-    });
-}, 30); */
