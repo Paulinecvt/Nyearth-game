89e5caeba84d253a4f2ee5b61b4604572fad5c02
Pauline Cvt <paulinecvt2021@gmail.com>
2024-01-30 13:08:53 +0100

    collision don't work but trashes have random icons now

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 // PLAYER CLASS
 class Player {
     constructor (){
-        this.positionX = 160;
+        this.positionX = 150;
         this.positionY = 300;
         this.width = 300;
         this.height = 150;
@@ -35,8 +35,33 @@ class Player {
         return this.positionY;
     };
 
+    moveRight(){
+        this.positionX += 30;
+        return this.positionX;
+    };
+
+    moveLeft (){
+        this.positionX -= 30;
+        return this.positionX;
+    }
+
 }; // end of player class
 
+
+// Array of image URLs for obstacles
+const obstacleImages = [
+    "./img/trash1.png",
+    "./img/trash2.png",
+    "./img/trash3.png",
+    "./img/trash4.png",
+    "./img/trash5.png"
+ ];
+ 
+ // Function to choose a random image URL
+ function chooseRandomImage() {
+    const randomIndex = Math.floor(Math.random() * obstacleImages.length);
+    return obstacleImages[randomIndex];
+ };
 
 
 
@@ -45,24 +70,24 @@ class Trashes {
     constructor() {
         this.width = 120;
         this.height = 120;
-        this.positionX = 100;
+        this.positionX = 1400;
         // this.positionX = window.innerWidth - this.width;
         this.positionY = Math.floor(Math.random() * (window.innerHeight - this.height));
-        // this.domElm = null;
+        this.imageSrc = chooseRandomImage();
 
         this.createDomElm();
-        
-    
     };
 
     // Create element of the trashes
     createDomElm() {
-        this.domElm = document.createElement("div");
+        this.domElm = document.createElement("img");
+        this.domElm.src = this.imageSrc;
         this.domElm.setAttribute("class", "trash");
         this.domElm.style.width = this.width + "px";
         this.domElm.style.height = this.height + "px";
+        this.domElm.style.top = this.positionY+ "px";
         this.domElm.style.left = this.positionX + "px";
-        this.domElm.style.bottom = this.positionY + "px";
+        
 
         const boardElm = document.getElementById("board");
         boardElm.appendChild(this.domElm);
@@ -70,16 +95,16 @@ class Trashes {
 
     // Move trashes to left
     moveLeft() {
-        this.positionX -= 10;
-        this.domElm.style.left = this.positionX + "%"
-        // console.log(this.positionX);
+        this.positionX -= 40;
+        this.domElm.style.left = this.positionX + "px"
+        return this.positionX;
       };
 
       // Function to remove obstacles that are marked for removal
      removeObstacles() {
         obstacles = obstacles.filter(obstacle => obstacle.positionX + obstacle.width > 0);
      };
- };
+ }; //end of trash class
        
 
 
@@ -108,18 +133,7 @@ function moveTrashes() {
      setInterval(() => {
         obstacles.forEach((obstacleInstance) => {
             obstacleInstance.moveLeft();
-            
-        });
-    }, 200);
-        obstacles.forEach((obstacleInstance) => {
-            obstacleInstance.removeObstacles();
-        })
 
-};
-
-    setInterval(() => {
-
-            obstacles.forEach((obstacleInstance) => {
             // Calculate the boundaries
             const obstacleLeft = obstacleInstance.positionX;
             const obstacleRight = obstacleInstance.positionX + obstacleInstance.width;
@@ -132,19 +146,32 @@ function moveTrashes() {
             const playerBottom = player.positionY + player.height;
 
             // Check for collision between player and obstacle
+            
             if (
                 playerLeft < obstacleRight &&
-                playerRight >= obstacleLeft &&
+                playerRight > obstacleLeft &&
                 playerTop < obstacleBottom &&
                 playerBottom > obstacleTop
-            ) {
-                    // Collision detected, trigger game over
+            ) {  // Collision detected, trigger game over
                     console.log("Game over");
-                    
-                }; 
-               
+                };   
+        });
+    }, 200);
+
+        obstacles.forEach((obstacleInstance) => {
+            obstacleInstance.removeObstacles();
+        })
+
+};
+
+    setInterval(() => {
+
+            obstacles.forEach((obstacleInstance) => {
+              
+              
     });
-}, 200);
+
+}, 100);
 
 
 
@@ -163,14 +190,24 @@ moveTrashes();
     document.addEventListener("keydown", (e) => {
     if (e.code === 'ArrowUp') {
         player.moveUp();
-        
     } else if (e.code === 'ArrowDown') {
         player.moveDown(); 
+    } else if (e.code === 'ArrowLeft'){
+        player.moveLeft();
+    } else if (e.code === 'ArrowRight'){
+        player.moveRight();
+
     }
+
+
+
     player.domElm.style.top = player.positionY + 'px';
+    player.domElm.style.bottom = player.positionY + 'px';
+    player.domElm.style.left = player.positionX + 'px';
+    player.domElm.style.right = player.positionX + 'px';
     
 });
 
 
-// toujours pas de collision == tailles ?? 
+// toujours pas de collision
 
